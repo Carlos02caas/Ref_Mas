c7e7b807418b4364ae50d38d675f543bf999c9b1
Carlos02caas <carlos_caas@hotmail.com>
2017-08-14 21:00:17 -0500

    Apps

---
+++ ref/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\carlo\Documents\GitHub\DjangoProjects\Ref_Mas\ref\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
